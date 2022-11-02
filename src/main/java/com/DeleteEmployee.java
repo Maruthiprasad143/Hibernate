ba4934b92df2364ed8a09e0596346429c95462e0
maruthiprasad450 <maruthiprasad450@gmail.com>
2022-11-02 21:21:05 +0530

    Delete Update

--- src/main/java/com/DeleteEmployee.java
+++ src/main/java/com/DeleteEmployee.java
@@ -1,5 +1,17 @@
 package com;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class DeleteEmployee {
 
+	public static void main(String[] args) {
+		Session se = ReferenceEmployee.getRecored();
+		Transaction tx = se.beginTransaction();
+		Maruthi em = (Maruthi)se.get(Maruthi.class, new Integer(3));
+		
+		se.delete(em);
+		tx.commit();
+		System.out.println("Record Will be Deleted");
+	}
 }
